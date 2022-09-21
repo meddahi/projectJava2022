40ccc750e85d48628cadcae9ca33c824ab7e7dc4
pc <pc@DESKTOP-MK4LSRF.home>
2022-09-21 16:31:55 +0300

    First Commit

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,39 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello word");
+		
+		String metim = "İlginizi çekebilir";
+		String altMetin = "Vade Süresi";
+		
+		System.out.println(altMetin);
+		
+		
+		int vade = 12;
+		System.out.println(vade);
+		double dolarDun = 18.32;
+		System.out.println(dolarDun);
+		
+		boolean dolarDustumu = false;
+	
+		String okYonu = "down.svg";
+		if (dolarDustumu) {
+			
+		} else {
+
+		}
+		
+		String[] krediler = {"Hızlı Kredi" , "Maaşını Halkbanktan alanlar" , "Mutlu Elemanlar"};
+		//System.out.println(krediler[0]);
+		//System.out.println(krediler[1]);
+		//System.out.println(krediler[2]);
+		
+		for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[it]);
+		}
+		
+	}
+
+}
